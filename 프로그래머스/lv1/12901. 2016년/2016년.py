f0d92812f0eea0030ef8d5273f46a65eb7317bc1
YNK99 <49948943+YNK99@users.noreply.github.com>
2022-11-02 14:39:37 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,16 +1,18 @@
 import copy
 
 def solution(a, b):
-    answer = ''
-    week = ['FRI','SAT','SUN','MON','TUE','WED','THU']
     days = 0
-    for i in range(1, a):
-        if i in [1, 3, 5, 7, 8, 10, 12]:
-            days += 31
-        elif i == 2:
-            days += 29
-        else:
-            days += 30
-    days += b
-    answer = (week[(days % 7) - 1])
-    return answer
+    weeks = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    month_31 = [1, 3, 5, 7, 8, 10, 12]
+    if a == 1:
+        days += b
+    else:
+        for i in range(1, a):
+            if i == 2:
+                days += 29
+            elif i in month_31:
+                days += 31
+            else:
+                days += 30   
+        days += b
+    return weeks[days%7]
