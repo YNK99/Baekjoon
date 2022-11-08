2035074616d7d3a69a6560fc7a5865873e559626
YNK99 <49948943+YNK99@users.noreply.github.com>
2022-11-08 12:30:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,4 +1,3 @@
 def solution(A,B):
-    sorted_list = sorted([sorted(A), sorted(B, reverse = True)])
-    list1, list2 = sorted_list[0], sorted_list[1]
+    list1, list2 = sorted(A), sorted(B, reverse = True)
     return sum([c[0] * c[1] for c in zip(list1, list2)])
