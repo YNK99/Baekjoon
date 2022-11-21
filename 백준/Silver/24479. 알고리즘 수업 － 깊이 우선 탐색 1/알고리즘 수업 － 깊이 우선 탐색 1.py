7ab9840a013082feea82bc1f8aa7bbdccca16281
YNK99 <49948943+YNK99@users.noreply.github.com>
2022-11-21 22:12:16 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 692 ms, Memory: 174124 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+# E = [[]] * N   이거 안됨. 얕은 복사? 첫번째 리스트에만 요소를 추가하고 싶어도 모든 리스트에 추가됨...
+visited = [0] * (N+1)
+graph = [[] for c in range(N+1)]
+
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[end] += [start]
+    graph[start] += [end]
+
+cnt = 1
+def dfs(graph, R, visited):
+    global cnt
+    visited[R] = cnt
+    cnt += 1
+    for i in sorted(graph[R]):
+        if visited[i] == 0:
+            dfs(graph, i, visited)
+
+dfs(graph, R, visited)
+print(*visited[1:], sep = '\n')
