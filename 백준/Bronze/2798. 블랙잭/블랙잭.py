6d404d3559a9f58f548c10c20cb7ded2b08a00f3
YNK99 <49948943+YNK99@users.noreply.github.com>
2022-12-07 23:01:01 +0900

    [Bronze II] Title: 블랙잭, Time: 336 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,13 @@
 N, M = map(int, input().split())
 cards = list(map(int, input().split()))
-card = cards
-choose = []
+max_sum = 0
 
-for a in cards:
-    for b in cards:
-        for c in cards:
-            if a + b + c <= M and len(set([a, b, c])) == 3:
-                choose.append(a + b + c)
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if i != j and i != k and j != k:
+                nums = cards[i] + cards[j] + cards[k]
+                if nums <= M and max_sum < nums:
+                    max_sum = nums
 
-print(max(choose))
+print(max_sum)
