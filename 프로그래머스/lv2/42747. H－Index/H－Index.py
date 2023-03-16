19fdfe3cb8cba718dc9cbe2c353d9d8f1108a133
YNK99 <49948943+YNK99@users.noreply.github.com>
2023-03-16 23:23:51 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    citations = sorted(citations)
+    length = len(citations)
+    
+    for i in range(length):
+        if citations[i] >= length - i:
+            return length - i
+    return 0
